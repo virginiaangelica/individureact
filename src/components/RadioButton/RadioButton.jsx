9449b6cc45aef5a07ed92f023fe0f9c892ac85fa
Tom <amdthomthomi@gmail.com>
2024-11-12 15:34:13 +0700

    materi 17 react hooks

--- src/components/RadioButton/RadioButton.jsx
+++ src/components/RadioButton/RadioButton.jsx
@@ -1,31 +1,35 @@
-import  { useState } from "react";
+import React, { useState } from 'react';
 
-export default function RadioButton({ options, defaultValue }) {
-  // Set the selected state to the defaultValue passed as prop or the first option if no default is provided
+export default function RadioButton({ options, defaultValue, onChange }) {
   const [selected, setSelected] = useState(defaultValue || options[0]?.value);
+
+  const handleSelectionChange = (value) => {
+    setSelected(value);
+    onChange(value); // Call onChange prop to update the parent component
+  };
 
   return (
     <div className="flex gap-4">
-      {options.map((option) => (
+      {options.map((option, index) => (
         <label
           key={option.value}
-          className="flex items-center cursor-pointer gap-2"
+          className="flex items-center cursor-pointer gap-2 inline-flex"
         >
           <input
             type="radio"
             name="customRadio"
             value={option.value}
             checked={selected === option.value}
-            onChange={() => setSelected(option.value)}
-            className="hidden" // Hide default radio button
+            onChange={() => handleSelectionChange(option.value)}
+            className="hidden"
           />
           <span
             className={`w-4 h-4 rounded-full flex items-center justify-center border ${
               selected === option.value
-                ? "bg-[#6173E6] border-[#6173E6]"
-                : "border-[#656666]"
+                ? 'bg-[#6173E6] border-[#6173E6]'
+                : 'border-[#656666]'
             }`}
-            style={{ width: "16px", height: "16px", borderWidth: "1px" }}
+            style={{ width: '16px', height: '16px', borderWidth: '1px' }}
           >
             {selected === option.value && (
               <span className="w-2.5 h-2.5 bg-[#6173E6] rounded-full"></span>
@@ -33,7 +37,7 @@ export default function RadioButton({ options, defaultValue }) {
           </span>
           <span
             className={`ml-2 ${
-              selected === option.value ? "text-[#081116]" : "text-[#656666]"
+              selected === option.value ? 'text-[#081116]' : 'text-[#656666]'
             }`}
           >
             {option.label}
